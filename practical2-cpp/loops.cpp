a8594eea69573fed6401601a8a3426f1def6b112
aryanjadhav <aryanjadhav964@gmail.com>
2026-01-04 21:38:29 +0530

    Some more practice is done

--- practical2-cpp/loops.cpp
+++ practical2-cpp/loops.cpp
@@ -15,13 +15,24 @@ int main() {
     //Write a program to find the factorial of a number.
     int n;
     long long factorial = 1;
-    cout << "Enter the number : ";
-    cin >> n;
+    // cout << "Enter the number : ";
+    // cin >> n;
 
     for (i = 1; i <= n; i++) {
         factorial = factorial * i;
     }
 
-    cout << " The factorial of " << n << " is : " << factorial << endl;
+    // cout << " The factorial of " << n << " is : " << factorial << endl;
+
+    //practice
+    int userNum;
+    cout << "Enter the number : ";
+    cin >> userNum;
+
+    int a = 0;
+    while ( a<= userNum) {
+        cout << a << " ";
+        a++;
+    }
     return 0;
 }
