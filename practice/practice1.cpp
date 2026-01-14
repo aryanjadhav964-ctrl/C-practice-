580c23019a2309fa6463ae0e6f8ffa32d471cfd3
aryanjadhav <aryanjadhav964@gmail.com>
2026-01-14 10:18:33 +0530

    character, number and star square Pattern done

--- practice/practice1.cpp
+++ practice/practice1.cpp
@@ -42,22 +42,75 @@ int main() {
     //     cout << "Grade ";
     // }
 
-    int day = 3;
-    switch(day) {
-        case 1 :
-        cout << " Monday";
-        break;
+    // int day = 3;
+    // switch(day) {
+    //     case 1 :
+    //     cout << " Monday";
+    //     break;
 
-        case 2 :
-        cout << " Tuesday";
-        break;
+    //     case 2 :
+    //     cout << " Tuesday";
+    //     break;
 
-        case 3 :
-        cout << " wednesday";
-        break;
+    //     case 3 :
+    //     cout << " wednesday";
+    //     break;
 
-        default :
-        cout << "Invalid day";
-    }
+    //     default :
+    //     cout << "Invalid day";
+    // }
+
+
+    //square pattern by using numbers
+    int n;
+    cout << "Enter the value of n : ";
+    cin >> n;
+//     for (int i = 1; i <= n; i++) {
+//         for(int j = 1; j <= n; j++) {
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+// //Star pattern
+//     cout << endl;
+
+//      for (int i = 1; i <= n; i++) {
+//         for(int j = 1; j <= n; j++) {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+//     cout << endl;
+// //character pattern
+//      for (int i = 0; i < n; i++) {
+//         char ch = 'A';
+//         for(int j = 0; j < n; j++) {
+//             cout << ch;
+//             ch = ch + 1;
+//         }
+//         cout << endl;
+//     }
+
+//another type 
+    // int num = 1;
+    // for (int i = 1; i <= n; i++) {
+    //     for(int j = 1; j <= n; j++) {
+    //         cout << num;
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+
+
+    //charecter version home work
+
+    // char ch = 'A';
+    // for (int i = 1; i <= n; i++) {
+    //     for(int j = 1; j <= n; j++) {
+    //         cout << ch;
+    //         ch = ch + 1;
+    //     }
+    //     cout << endl;
+    // }
     return 0;
 }
