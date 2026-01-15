725b3e6923729491f69b1a89976e3230f3259b70
aryanjadhav <aryanjadhav964@gmail.com>
2026-01-15 23:57:54 +0530

    Loops practice on trangle pattern

--- practice/practice1.cpp
+++ practice/practice1.cpp
@@ -112,5 +112,63 @@ int main() {
     //     }
     //     cout << endl;
     // }
+
+    // int n;
+    // cout << " Enter the number n : ";
+    // cin >> n;
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < i+1; j++) {
+    //         cout << " * ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < i+1; j++) {
+    //         cout << i+1;
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 1; j <= i+1; j++) {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = i+1; j > 0; j--) {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+//Floyd's trangle pattern
+    // int num = 1;
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 1; j <= i+1; j++) {
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+
+
+    int num=1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout <<  " ";
+        }
+
+        //num
+        for (int j=0; j<n-i; j++) {
+            cout << (i+1);
+        }
+
+        cout << endl;
+    }
     return 0;
 }
