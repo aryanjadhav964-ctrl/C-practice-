0da25e5b344916b88df58049b324f6eeb9adace7
aryanjadhav <aryanjadhav964@gmail.com>
2026-01-22 18:43:15 +0530

    arrays,conditional statements and loops practice done [ for logic building ].

--- practical1-cpp/conditional1.cpp
+++ practical1-cpp/conditional1.cpp
@@ -19,16 +19,68 @@ int main () {
 
     //Write a programto check whether the number is positive, negative or zero.
 
-    int number;
-    cout << "Enter the number : ";
-    cin >> number;
+    // int number;
+    // cout << "Enter the number : ";
+    // cin >> number;
 
-    if (number == 0) {
-        cout << number << " is Zero" << endl; 
-    } else if (number >= 0) {
-        cout << number << " is Positive" << endl;
-    } else {
-        cout << number << " is Negative" << endl;
+    // if (number == 0) {
+    //     cout << number << " is Zero" << endl; 
+    // } else if (number >= 0) {
+    //     cout << number << " is Positive" << endl;
+    // } else {
+    //     cout << number << " is Negative" << endl;
+    // }
+
+    //odd and and even  
+    // if (num % 2 == 0) {
+    //     cout << num << " is a even num." << endl;
+    // } else {
+    //     cout << num << " is a odd number." << endl;
+    // }
+
+
+    //loops ========> 
+    //sum of nums from 1 to 100
+    double sum = 0;
+    for (int i=1; i<=100; i++) {
+        sum += i;
     }
+    // cout << "Sum is : " << sum << endl;
+
+    //factoril of a number  
+    double fact = 1;
+    for (int i=1; i<=num; i++) {
+        fact *= i;
+    }
+    // cout << "Factorial is : " << fact << endl;
+
+
+    //larget number of array
+    int n;
+    // cout << "Enter the size of array : \n";
+    // cin >> n;
+    
+    int array[n];
+
+    // cout << "Enter the array elements : \n";
+    // for (int i = 0; i < n; i++ ) {
+    //     cin >> array[i];
+    // }
+
+    int largest = array[0];
+
+    for (int i=0; i<n; i++) {
+        if (array[i] > largest) {
+            largest = array[i];
+        }
+    }
+
+
+
+    // cout << "Largest value of array is : " << largest << endl;
+
+    //pointer ===>
+
+    //A pointer is a variable that stores the address of another variable
     return 0;
 }
