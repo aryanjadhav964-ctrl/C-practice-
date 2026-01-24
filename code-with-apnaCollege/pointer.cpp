600d7af1f5d12a084077ac6fd95420efb39207d0
aryanjadhav <aryanjadhav964@gmail.com>
2026-01-24 13:02:40 +0530

    Array pointer, pass by value using pointer and alias and pointer arithmetic

--- code-with-apnaCollege/pointer.cpp
+++ code-with-apnaCollege/pointer.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
+void changes(int* ptr) {//pass by reference usnign pointer[ make changes in original value]
+    *ptr = 20;
+}
+void changeA(int &b) {//pass by reference using alias[ make changes in original value]
+    b = 30;
+}
+
 int main() {
 
     int a=10;
@@ -17,15 +24,36 @@ int main() {
     double sum = **(varPtrPro1) + **(varPtrPro2); //adition
     double sub = **(varPtrPro1) - **(varPtrPro2); //subtraction
     // cout << "The sum is : " << sum << endl;
-    cout << "The subtraction is : " << sub << endl;
+    // cout << "The subtraction is : " << sub << endl;
 
-    //ponter with array
+    //ponter with array [ Arrays are constant pointers] 
     int arr[3] = {10,20,30};
-    int *ptrArray = arr;
+    int *ptrArray = arr; //ptrArray == index 0
+    ptrArray++; //ptrArrya starts form next index 1[ adds 1 byte in address]
+    cout << *ptrArray << endl;
+
+    int newarr[] = {55,44,33,22,11};
+    // cout << *newarr << endl; //index 0 value 
+    // cout << *(newarr+1) << endl; // index 1 value
+    // cout << *newarr+1 << endl;//adds 1 in index 0 value 
+
 
     // cout << *(ptrArray) << endl; //index 0 value will print
     // cout << *(ptrArray+1) << endl; //index 1 value will print
     // cout << *(ptrArray+2) << endl; //index 2 value will print
+
+    //pass by referance using pointer 
+    // int a = 10;
+    changes(&a);
+    // cout << a << endl;
+    //pass by referance using alias
+    changeA(a);
+    // cout << a << endl;
+
+
+    //pointer arithmatics
+
+
 
     return 0;
 } 
