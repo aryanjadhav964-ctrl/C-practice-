f34bd1b86819e1f37d2c57824b54c6f0b1f2549c
aryanjadhav <aryanjadhav964@gmail.com>
2026-01-30 20:39:32 +0530

    Infinite looping problem sort and added more features.

--- project2026cpp/numberUtilityTool/tool.cpp
+++ project2026cpp/numberUtilityTool/tool.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 void displayMenu();
-double sumOfDigits(int n);
+int sumOfDigits(int n);
+int factorial(int n);
 
 int main() {
     cout << "======================================" << endl;
@@ -9,71 +11,111 @@ int main() {
     cout << "You are using Number Utility Tool." << endl;
     cout << "======================================" << endl;
 
-    int choise;
+    int choice;
     int number;
+    int ans;
+
+    displayMenu();
 
     do {
-        displayMenu();
 
         //to get chose
-        cout << "Enter you choise between [1-5] : " << endl;
-        cin >> choise;
+        cout << "Enter your choise between [1-6] : " << endl;
+        cin >> choice;
 
-        switch(choise) {
+        if (cin.fail()) {
+        cin.clear(); // clear error flag
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input! Please enter a number between 1 to 6.\n";
+        continue;
+    }
+        
+
+        switch(choice) {
             case 1: {
             //call sumOfDigit function
             cout << "---Sum of digits---" << endl;
             cout << "Enter the Number : ";
             cin >> number;
-                cout << endl;
+            cout << " " << endl;
             int result = sumOfDigits(number);
-            cout << "Sum of digits of" << number << " = " << result << endl;
+            cout << "Sum of digits of " << number << " = " << result << endl;
+            cout << " " << endl;
             break;
             }
 
-            case 2:
+            case 2: {
             //call factorial function
+            cout << "--- Factorial of number---" << endl;
+            cout << "Enter the Number : ";
+            cin >> number;
+            cout << " " << endl;
+            int result = factorial(number);
+            cout << "Factorial of " << number << " = " << result << endl;
+            cout << " " << endl;
             break;
+            }
 
-            case 3:
+            case 3:{
             //call multipilaction function
+            cout << "Currently this function is not working!" << endl;
+            cout << "It will start soon." << endl;
+            cout << "You can kindly enter 6 and EXIT." << endl;
+            cout << " " << endl;
             break;
+            }
 
-            case 4:
+            case 4:{
             //call subtraction function
+            cout << "Currently this function is not working!" << endl;
+            cout << "It will start soon." << endl;
+            cout << "You can kindly enter 6 and EXIT." << endl;
+            cout << " " << endl;
             break;
+            }
 
-            case 5:
+            case 5:{
             //call palindrome function
+            cout << "Currently this function is not working!" << endl;
+            cout << "It will start soon." << endl;
+            cout << "You can kindly enter 6 and EXIT." << endl;
+            cout << " " << endl;
             break;
+            }
 
-            case 6 :
+            case 6 :{
             //exit
+            choice = 6;
             break;
+            }
 
-            default :
+            default : {
             cout << "You entered invalid number! Try agian." << endl;
+            cout << " " << endl;
             break;
+            }
         }
 
-    } while(choise = 6);
+    } while(choice != 6);
 }
 
 void displayMenu() {
     cout << "===============================" << endl;
-    cout << "               main mannu      " << endl;
+    cout << "               main menu      " << endl;
     cout << "===============================" << endl;
-    cout << "1. Sum of numbers" << endl;
+    cout << "1. Sum of digits" << endl;
     cout << "2. Factorial finder" << endl;
     cout << "3. Multiplication of numbers" << endl;
     cout << "4. Subtraction of numbers" << endl;
-    cout << "5. Sum of digits" << endl;
-    cout << "6. Is number palindrome?" << endl;
-    cout << "7. Exit" << endl;
+    cout << "5. Is number palindrome?" << endl;
+    cout << "6. Exit" << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
 
 }
 //1 : sumOfDigits function 
-double sumOfDigits(int n) {
+int sumOfDigits(int n) {
     n = abs(n); //handle negative values
 
     int sum = 0;
@@ -83,4 +125,14 @@ double sumOfDigits(int n) {
     }
     return sum;
 
+}
+
+//2 : Factorial function
+int factorial(int n) {
+    n = abs(n);
+    int fact = 1;
+    for (int i=1; i<=n; i++) {
+        fact *= i;
+    }
+    return fact;
 }
